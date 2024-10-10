e4d0382c9ea6be42d7ce3acf9cae4435ec487d94
zanfirD <159519161+zanfirD@users.noreply.github.com>
2024-10-10 08:57:21 +0300

    Update locatie.js

--- locatie.js
+++ locatie.js
@@ -1,29 +1,29 @@
-get_geo_position();
-function on_succes(get_geo_position) {
-    document.getElementById("latitude").innerHTML = "Latitude = " + SVGTextPositioningElement.coords.latitude;
-    document.getElementById("longitude").innerHTML = "Longitude = " + SVGTextPositioningElement.coords.longitude;
-    document.getElementById("acc").innerHTML = "Accuracy = " + SVGTextPositioningElement.coords.accuracy;
-    document.getElementById("altitude").innerHTML = "Altitude = " + SVGTextPositioningElement.coords.altitude;
-    var latlon = position.coords.latitude + "," + position.coords.longitude;
-
-    var img_url = "https://maps.googleapis.com/maps/api/staticmap?center="+latlon+"&zoom=14&size=400x300&key=AIzaSyCkPur0Ce3ZXvRlxJMYSgZzx1zXy6Y-QIk";
-
-    document.getElementById("map").innerHTML = "<img src='"+img_url+"'>";
+function get_geo_position() {
+    var geo = navigator.geolocation;
+    geo.getCurrentPosition(on_success, on_error, geo_params);
 }
 
-function on_error(e)
-{
-    document.getElementById("text").innerHTML = e.message;
+function on_success(position) {
+    document.getElementById("latitude").innerHTML = "Latitude = " + position.coords.latitude;
+    document.getElementById("longitude").innerHTML = "Longitude = " + position.coords.longitude;
+    document.getElementById("acc").innerHTML = "Accuracy = " + position.coords.accuracy;
+    document.getElementById("altitude").innerHTML = "Altitude = " + (position.coords.altitude ? position.coords.altitude : 'Not available');
+
+    var latlon = position.coords.latitude + "," + position.coords.longitude;
+
+    var img_url = "https://maps.googleapis.com/maps/api/staticmap?center=" + latlon + "&zoom=14&size=400x300&key=AIzaSyCkPur0Ce3ZXvRlxJMYSgZzx1zXy6Y-QIk";
+
+    document.getElementById("map").innerHTML = "<img src='" + img_url + "'>";
+}
+
+function on_error(e) {
+    document.getElementById("latitude").innerHTML = "Error: " + e.message;
 }
 
 var geo_params = {
-    enableHighAccuracy:true,
-    timeout:1000,
-    maximumAge:0
+    enableHighAccuracy: true,
+    timeout: 10000,
+    maximumAge: 0
 };
 
-function get_geo_position()
-{
-    var geo = navigator.geolocation;
-    geo.getCurrentPosition(on_succes, on_error, geo_params)
-}
+get_geo_position();
